8068936530926bec1175d35d273f6a536adef73d
miscmike <miscmikesyd@gmail.com>
2022-10-02 20:32:34 +1100

    very hacky jump

--- Sprite.gd
+++ Sprite.gd
@@ -1,15 +1,14 @@
 extends KinematicBody2D
 
-const GRAVITY = 400.0
+const GRAVITY = 100.0
 const WALK_SPEED = 200
 const ROTATION_SPEED = 600
 
 var velocity = Vector2()
-onready var anim = $AnimationPlayer
 
 func _input(event):
 	if event.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = -GRAVITY
+		print("up)")
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
@@ -25,7 +24,7 @@ func _physics_process(delta):
 	
 	move_and_slide(velocity, Vector2(0, -1))
 	
-	anim.play("clock_ticking")
+
 
 
 
