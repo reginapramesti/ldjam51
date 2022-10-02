6fde6de1d5411cd1614bbab300098a039f05e91d
Regina Pramesti <regina.pramesti.8@gmail.com>
2022-10-02 21:35:43 +1100

    Remove random tutorial scenes (#8)

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -22,33 +22,4 @@ func _integrate_forces(state):
 		if (gravity.transform.origin.distance_to(self.transform.origin) < 190):
 			apply_central_impulse(-jump*(gravity.transform.origin - self.transform.origin))
 
-	
-		
 
-#var move_direction = Vector2()
-#var last_strong_direction = Vector2()
-#var local_gravity = Vector2()
-#onready var model = get_node(".")
-#
-#func _integrate_forces(state):
-#
-#	local_gravity = state.total_gravity.normalized()
-#	print(local_gravity)
-#
-#	move_direction = get_model_oriented_input()
-#
-#	orient_character_to_direction(last_strong_direction, state.step)
-#
-#func get_model_oriented_input():
-#	var input_left_right = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-#	var input_up = Input.get_action_strength("ui_up")
-#
-#	return Vector2(input_left_right, 0)
-#
-#func orient_character_to_direction(direction, delta):
-#	var left_axis = -local_gravity.cross(direction)
-#	var rotation_basis = Basis(left_axis, -local_gravity, direction).orthonormalized()
-#	model.transform.basis = model.transform.basis.get_rotation_quat().slerp(
-#		rotation_basis, delta * rotation_speed
-#	)
-#	pass
