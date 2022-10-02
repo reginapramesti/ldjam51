238c9730a2b13272a3d2464b659df8136e7c7cc2
miscmike <miscmikesyd@gmail.com>
2022-10-02 17:03:35 +1100

    add jump on ground only

--- Sprite.gd
+++ Sprite.gd
@@ -4,9 +4,10 @@ const GRAVITY = 400.0
 const WALK_SPEED = 200
 
 var velocity = Vector2()
+var rng = RandomNumberGenerator.new()
 
 func _input(event):
-	if event.is_action_pressed("ui_up"):
+	if event.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -GRAVITY
 
 func _physics_process(delta):
