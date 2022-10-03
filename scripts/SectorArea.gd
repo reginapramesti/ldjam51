ca47145ac8974c0f8cd3f1ff792bb73ef7eaef81
Regina Pramesti <regina.pramesti.8@gmail.com>
2022-10-03 17:02:12 +1100

    Some cleanup and renaming of nodes (#13)

--- scripts/SectorArea.gd
+++ scripts/SectorArea.gd
@@ -8,7 +8,6 @@ func _ready():
 	timer.connect("timeout", self, "update_rotation")
 	timer.wait_time = wait_time
 	timer.start()
-	print(timer)
 	
 func update_rotation():
 	rotate(rotation_increments)
